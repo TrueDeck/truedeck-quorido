b51182fd9bdadd1bd71433522c73b85d255684a8
truedeckdev <truedeck@protonmail.com>
2019-08-13 18:23:59 +0530

    WithdrawerRole contract created for access to Game Contracts to withdraw tokens from bankroll.

---
+++ contracts/WithdrawerRole.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.5.0;
+
+import "zos-lib/contracts/Initializable.sol";
+import "openzeppelin-eth/contracts/access/Roles.sol";
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+import "./IGame.sol";
+
+/**
+ * @title WithdrawerRole
+ * @dev Withdrawer IGame Contracts have been approved by the Owner to perform certain actions.
+ * This role is special in that:
+ * - Only IGame Contracts can be added as Withdrawers.
+ * - The only account that can manage this role is Owner, and not Withdrawers themselves.
+ */
+contract WithdrawerRole is Initializable, Ownable {
+    using Roles for Roles.Role;
+
+    event WithdrawerAdded(address indexed account);
+    event WithdrawerRemoved(address indexed account);
+
+    Roles.Role private _withdrawers;
+
+    modifier onlyWithdrawer() {
+        require(isWithdrawer(msg.sender));
+        _;
+    }
+
+    function initialize(address sender) public initializer {
+        Ownable.initialize(sender);
+    }
+
+    function isWithdrawer(address account) public view returns (bool) {
+        return _withdrawers.has(account);
+    }
+
+    function addWithdrawer(address account) public onlyOwner {
+        _addWithdrawer(account);
+    }
+
+    function removeWithdrawer(address account) public onlyOwner {
+        _removeWithdrawer(account);
+    }
+
+    function renounceWithdrawer() public {
+        _removeWithdrawer(msg.sender);
+    }
+
+    function _addWithdrawer(address account) internal {
+        IGame game = IGame(account);
+        require(game.isGame());
+
+        _withdrawers.add(account);
+        emit WithdrawerAdded(account);
+    }
+
+    function _removeWithdrawer(address account) internal {
+        _withdrawers.remove(account);
+        emit WithdrawerRemoved(account);
+    }
+
+    uint256[50] private ______gap;
+}
