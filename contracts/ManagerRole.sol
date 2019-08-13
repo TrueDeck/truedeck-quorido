b0a6d8deeb476d7507077709278809062bb5581a
truedeckdev <truedeck@protonmail.com>
2019-08-13 17:54:59 +0530

    ManagerRole contract created to provide role access management to Owner.

---
+++ contracts/ManagerRole.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.5.0;
+
+import "zos-lib/contracts/Initializable.sol";
+import "openzeppelin-eth/contracts/access/Roles.sol";
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+
+/**
+ * @title ManagerRole
+ * @dev Manager accounts have been approved by the Owner to perform certain actions.
+ * This role is special in that the only account that can manage this role is Owner,
+ * and not Managers themselves.
+ */
+contract ManagerRole is Initializable, Ownable {
+    using Roles for Roles.Role;
+
+    event ManagerAdded(address indexed account);
+    event ManagerRemoved(address indexed account);
+
+    Roles.Role private _managers;
+
+    modifier onlyManager() {
+        require(isManager(msg.sender));
+        _;
+    }
+
+    function initialize(address owner) public initializer {
+        Ownable.initialize(owner);
+    }
+
+    function isManager(address account) public view returns (bool) {
+        return _managers.has(account);
+    }
+
+    function addManager(address account) public onlyOwner {
+        _addManager(account);
+    }
+
+    function removeManager(address account) public onlyOwner {
+        _removeManager(account);
+    }
+
+    function renounceManager() public {
+        _removeManager(msg.sender);
+    }
+
+    function _addManager(address account) internal {
+        _managers.add(account);
+        emit ManagerAdded(account);
+    }
+
+    function _removeManager(address account) internal {
+        _managers.remove(account);
+        emit ManagerRemoved(account);
+    }
+
+    uint256[50] private ______gap;
+}
