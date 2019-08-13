275107c0eb1f30b1bfc85c2bdbbb5218e3052776
truedeckdev <truedeck@protonmail.com>
2019-08-13 18:22:50 +0530

    IGame interface created for all Game Contract implementations.

---
+++ contracts/IGame.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title IGame interface
+*/
+interface IGame {
+
+    function isGame() public returns (bool);
+
+    function deposit(IERC20 token, uint256 amount) external returns (bool);
+
+    function withdraw(
+        IERC20 token,
+        uint256 amount,
+        uint256[] calldata nonce,
+        uint256[] calldata wonIndexes,
+        bytes32[] calldata clientSeed,
+        bytes32[] calldata serverSeed,
+        bytes calldata data,
+        bytes calldata proof
+    ) external returns (bool);
+
+    event Withdrawal(
+        address indexed player,
+        bytes proof,
+        uint256 amount,
+        uint256 balance
+    );
+
+}
