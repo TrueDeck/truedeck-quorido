a6ced66132356128f9242a988506e41e7968b35d
truedeckdev <truedeck@protonmail.com>
2019-08-17 23:24:02 +0530

    Profiling added for Utils conversion functions.

--- packages/contracts-solidity/migrations/2_deploy_contracts.js
+++ packages/contracts-solidity/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Counter = artifacts.require("Counter");
+const Utils = artifacts.require("UtilsMock");
 
 module.exports = function(deployer) {
   deployer.deploy(Counter);
+  deployer.deploy(Utils);
 };
