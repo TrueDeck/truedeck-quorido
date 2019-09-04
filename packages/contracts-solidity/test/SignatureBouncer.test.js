8913ba5687ccf78bbbcb8c1d8ecc59579a903c14
truedeckdev <truedeck@protonmail.com>
2019-09-04 18:11:03 +0530

    Tests created for signature validation of data.

---
+++ packages/contracts-solidity/test/SignatureBouncer.test.js
@@ -0,0 +1,30 @@
+const { shouldFail } = require('openzeppelin-test-helpers');
+const { signMessage } = require('./helpers');
+const { shouldBehaveLikePublicRole } = require('openzeppelin-eth/test/behaviors/access/roles/PublicRole.behavior');
+const should = require('chai').should();
+
+const SignatureBouncerMock = artifacts.require('SignatureBouncerMock');
+
+contract('SignatureBouncer', function ([_, signer, otherSigner, anyone, authorizedUser, ...otherAccounts]) {
+    beforeEach(async function () {
+        this.sigBouncer = await SignatureBouncerMock.new({ from: signer });
+    });
+
+    context('signature validation', function () {
+        it('invalidates wrong hash', async function () {
+            const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+            const invalid_hash = "0xabcd48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+            (await this.sigBouncer.checkValidDataHash(h1, await signMessage(signer, invalid_hash))).should.equal(false);
+        });
+
+        it('invalidates hash by unauthorized signer', async function () {
+            const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+            (await this.sigBouncer.checkValidDataHash(h1, await signMessage(otherSigner, h1))).should.equal(false);
+        });
+
+        it('validates hash by authorized signer', async function () {
+            const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+            (await this.sigBouncer.checkValidDataHash(h1, await signMessage(signer, h1))).should.equal(true);
+        });
+    });
+});
--- packages/contracts-solidity/test/helpers.js
+++ packages/contracts-solidity/test/helpers.js
@@ -24,9 +24,27 @@ function calculateGameHash(prevhash, serverSeed, clientData) {
     return gamehash;
 }
 
+function fixSignature (signature) {
+  // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+  // signature malleability if version is 0/1
+  // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+  let v = parseInt(signature.slice(130, 132), 16);
+  if (v < 27) {
+    v += 27;
+  }
+  const vHex = v.toString(16);
+  return signature.slice(0, 130) + vHex;
+}
+
+// signs message in node (ganache auto-applies "Ethereum Signed Message" prefix)
+async function signMessage (signer, messageHex = '0x') {
+  return fixSignature(await web3.eth.sign(messageHex, signer));
+};
+
 module.exports = {
     initializeChip,
     initializeBankroll,
     initializeDice,
-    calculateGameHash
+    calculateGameHash,
+    signMessage
 };
