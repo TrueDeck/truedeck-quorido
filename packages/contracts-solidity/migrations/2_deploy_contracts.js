d65a1d0af5f2ef4c253d0a516bea1e65591dcdc9
truedeckdev <truedeck@protonmail.com>
2019-09-14 10:05:36 +0530

    Chip, Bankroll & Dice added to deployer and zos.json

--- packages/contracts-solidity/migrations/2_deploy_contracts.js
+++ packages/contracts-solidity/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const Utils = artifacts.require("UtilsMock");
+const Chip = artifacts.require("Chip");
+const Bankroll = artifacts.require("Bankroll");
+const Dice = artifacts.require("Dice");
 
 module.exports = function(deployer) {
-  deployer.deploy(Utils);
+    deployer.deploy(Utils);
+    deployer.deploy(Chip);
+    deployer.deploy(Bankroll);
+    deployer.deploy(Dice);
 };
