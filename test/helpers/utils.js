fa678d45575e9b0884e0be90ba36757b4f07a2ef
truedeckdev <truedeck@protonmail.com>
2019-09-14 17:02:45 +0530

    test helpers restructured.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,22 +1,4 @@
 
-async function initializeChip(contract, name, symbol, decimals, initialSupply, initialHolder, minters, pausers, from) {
-    const signature = 'initialize(string,string,uint8,uint256,address,address[],address[])';
-    const args = [name, symbol, decimals, initialSupply, initialHolder, minters, pausers];
-    await contract.methods[signature](...args, { from });
-}
-
-async function initializeBankroll(contract, owner, from) {
-    const signature = 'initialize(address)';
-    const args = [owner];
-    await contract.methods[signature](...args, { from });
-}
-
-async function initializeDice(contract, owner, signer, bankroll, from) {
-    const signature = 'initialize(address,address,address)';
-    const args = [owner, signer, bankroll];
-    await contract.methods[signature](...args, { from });
-}
-
 function calculateGameHash(prevhash, serverSeed, clientData) {
     const gamehash = web3.utils.keccak256(web3.eth.abi.encodeParameters(
                             ["bytes32", "bytes32", "bytes"],
@@ -42,9 +24,6 @@ async function signMessage (signer, messageHex = '0x') {
 };
 
 module.exports = {
-    initializeChip,
-    initializeBankroll,
-    initializeDice,
     calculateGameHash,
     signMessage
 };
