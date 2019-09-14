e223f2665fa2105c1837a9d4f8b262623939f5d0
truedeckdev <truedeck@protonmail.com>
2019-09-14 21:56:24 +0530

    Encoder created for Dice Game Data.

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,10 +1,12 @@
 const { initializeBankroll, initializeChip, initializeDice } = require('./initializers');
-const { calculateGameHash, signMessage } = require('./utils.js');
+const { calculateGameHash, signMessage } = require('./utils');
+const { DiceGameDataEncoder } = require('./encoders');
 
 module.exports = {
     initializeBankroll,
     initializeChip,
     initializeDice,
     calculateGameHash,
-    signMessage
-}
+    signMessage,
+    DiceGameDataEncoder
+};
