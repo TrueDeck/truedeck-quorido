9ba67ad58d983aee73e74cc66f1176a7ba8c45df
truedeckdev <truedeck@protonmail.com>
2019-09-14 10:06:31 +0530

    Profile test created for a simple dice game.

--- test/SignatureBouncer.test.js
+++ test/SignatureBouncer.test.js
@@ -5,26 +5,41 @@ const should = require('chai').should();
 
 const SignatureBouncerMock = artifacts.require('SignatureBouncerMock');
 
+const mode = process.env.MODE;
+
 contract('SignatureBouncer', function ([_, signer, otherSigner, anyone, authorizedUser, ...otherAccounts]) {
-    beforeEach(async function () {
-        this.sigBouncer = await SignatureBouncerMock.new({ from: signer });
+
+    after("write coverage output", async () => {
+        if (mode === "coverage") {
+            await global.coverageSubprovider.writeCoverageAsync();
+        }
     });
 
-    context('signature validation', function () {
-        it('invalidates wrong hash', async function () {
-            const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
-            const invalid_hash = "0xabcd48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
-            (await this.sigBouncer.checkValidDataHash(h1, await signMessage(signer, invalid_hash))).should.equal(false);
-        });
+    if (mode !== "profile") {
+        describe("tests/coverage", () => {
 
-        it('invalidates hash by unauthorized signer', async function () {
-            const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
-            (await this.sigBouncer.checkValidDataHash(h1, await signMessage(otherSigner, h1))).should.equal(false);
-        });
+            beforeEach(async function () {
+                this.sigBouncer = await SignatureBouncerMock.new({ from: signer });
+            });
 
-        it('validates hash by authorized signer', async function () {
-            const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
-            (await this.sigBouncer.checkValidDataHash(h1, await signMessage(signer, h1))).should.equal(true);
+            context('signature validation', function () {
+                it('invalidates wrong hash', async function () {
+                    const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+                    const invalid_hash = "0xabcd48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+                    (await this.sigBouncer.checkValidDataHash(h1, await signMessage(signer, invalid_hash))).should.equal(false);
+                });
+
+                it('invalidates hash by unauthorized signer', async function () {
+                    const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+                    (await this.sigBouncer.checkValidDataHash(h1, await signMessage(otherSigner, h1))).should.equal(false);
+                });
+
+                it('validates hash by authorized signer', async function () {
+                    const h1 = "0xc5ee48380eecc4832905500b23878950f5c00bf985086a4552d31285fcaf4519";
+                    (await this.sigBouncer.checkValidDataHash(h1, await signMessage(signer, h1))).should.equal(true);
+                });
+            });
+
         });
-    });
+    }
 });
