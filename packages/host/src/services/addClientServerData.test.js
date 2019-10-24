9125e7894a743aaf39615f7c5ae64c0f38ddf8dc
truedeckdev <truedeck@protonmail.com>
2019-10-24 09:00:30 +0530

    Test added for multiple entries in addClientServerData service of @truedeck/quorido-host.

--- packages/host/src/services/addClientServerData.test.js
+++ packages/host/src/services/addClientServerData.test.js
@@ -54,7 +54,7 @@ describe("addClientServerData", function() {
       const clientDataHash = getRandomHex(32)
       const serverSeed = getRandomHex(32)
 
-      const dateMock = new Date("2019-05-14T11:01:58.135Z")
+      const dateMock = new Date("2019-10-01T12:00:00.000Z")
       jest
         .spyOn(global.Date, "now")
         .mockImplementationOnce(() => dateMock.valueOf())
@@ -77,6 +77,50 @@ describe("addClientServerData", function() {
         attr1: clientDataHash,
         attr2: serverSeed,
       })
+    })
+
+    it("adds multiple entries", async function() {
+      const numberOfEntries = 5
+      const player = getRandomAddress()
+      const game = getRandomAddress()
+      const token = getRandomAddress()
+      const clientDataHashes = []
+      const serverSeeds = []
+      for (let i = 0; i < numberOfEntries; i++) {
+        clientDataHashes.push(getRandomHex(32))
+        serverSeeds.push(getRandomHex(32))
+      }
+
+      for (let i = 0; i < numberOfEntries; i++) {
+        await addClientServerData(
+          player,
+          game,
+          token,
+          clientDataHashes[i],
+          serverSeeds[i]
+        )
+      }
+
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        KeyConditionExpression: "#pk = :pk",
+        ExpressionAttributeNames: {
+          "#pk": "pk",
+        },
+        ExpressionAttributeValues: {
+          ":pk": `${player}#${game}#${token}#hash`,
+        },
+      }
+      const { Items } = await ddb.query(params).promise()
+
+      for (let i = 0; i < numberOfEntries; i++) {
+        expect(Items[i]).toEqual({
+          pk: `${player}#${game}#${token}#hash`,
+          sk: expect.any(String),
+          attr1: clientDataHashes[i],
+          attr2: serverSeeds[i],
+        })
+      }
     })
   })
 })
