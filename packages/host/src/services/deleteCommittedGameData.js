b43819df626047f8476e7cc1121f474da95759d2
truedeckdev <truedeck@protonmail.com>
2019-10-26 12:23:39 +0530

    deleteCommittedGameData service created in @truedeck/quorido-host.

---
+++ packages/host/src/services/deleteCommittedGameData.js
@@ -0,0 +1,26 @@
+import ddb from "./dynamoDb"
+import getCommittedGameData from "./getCommittedGameData"
+
+async function deleteCommittedGameData(player, game, token) {
+  const { Items } = await getCommittedGameData(player, game, token)
+
+  const requests = [];
+  for (let i = 0; i < Items.length; i++) {
+    requests.push({
+      DeleteRequest: {
+        Key: {
+          "pk": Items[i].pk,
+          "sk": Items[i].sk,
+        }
+      }
+    });
+  }
+  const params = {
+    RequestItems: {
+      [process.env.DYNAMODB_TABLE]: requests
+    }
+  }
+  return ddb.batchWrite(params).promise()
+}
+
+export default deleteCommittedGameData
